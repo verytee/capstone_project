0be7d6764acf7153aa4b07c54d4eb71f1c9a7ae5
Verity Allison <verityalls@gmail.com>
2026-02-10 11:50:17 +0000

    add allauth url

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('', include('booking.urls')),
 ]
