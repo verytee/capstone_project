6237a530748633e4bf4812f3bbee43bc4c0c6bca
Verity Allison <verityalls@gmail.com>
2026-02-10 13:20:24 +0000

    add logic so dates that have passed cannot be booked

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,7 @@
+from operator import add
 from django.shortcuts import render
 from django.views import generic
+from django.utils import timezone
 from .models import Room, RoomBooking
 
 # Create your views here.
@@ -7,6 +9,13 @@ from .models import Room, RoomBooking
 class RoomList(generic.ListView):
     model = Room
     template_name = 'booking/index.html'
+
+# add logic so dates in the past are not shown as available for booking
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['today'] = timezone.now().date()
+        return context
 
 
 class BookingList(generic.ListView):
