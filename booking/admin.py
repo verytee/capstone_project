47bbcea8ab0fa568cee7381e73c210b74d38117c
Verity Allison <verityalls@gmail.com>
2026-02-10 12:43:25 +0000

    make summernote safe for html to render

--- booking/admin.py
+++ booking/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Room, RoomBooking
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-admin.site.register(Room)
 admin.site.register(RoomBooking)
+
+@admin.register(Room)
+class RoomAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description',)
