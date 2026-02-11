9d2f3bea292f0d97b12b2e21c51c5ba2a3051fac
Verity Allison <verityalls@gmail.com>
2026-02-11 09:33:45 +0000

    add list of bookings code to manage-bookings

--- booking/urls.py
+++ booking/urls.py
@@ -5,4 +5,5 @@ from .views import BookingList, RoomList
 urlpatterns = [
     path('', RoomList.as_view(), name='room_list'),
     path('bookings/', BookingList.as_view(), name='booking_list'),
+    path('manage-bookings/', BookingList.as_view(), name='manage_bookings'),
 ]
