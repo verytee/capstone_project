4f0b1dcff743f82b5975eea2c85fc3b6215fee8b
Verity Allison <verityalls@gmail.com>
2026-02-13 11:53:08 +0000

    add about view

--- about/views.py
+++ about/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import AboutUs
 
 # Create your views here.
+
+def about_us(request):
+# View to display the About Us page with hotel information.
+    about_info = AboutUs.objects.first()
+    return render(request, 'about/about-us.html', 
+                  {'about_info': about_info})
