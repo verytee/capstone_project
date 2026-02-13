7dc972483c05b4923bece830898ade807baf9f95
Verity Allison <verityalls@gmail.com>
2026-02-13 12:22:37 +0000

    set up about page

--- about/views.py
+++ about/views.py
@@ -4,7 +4,6 @@ from .models import AboutUs
 # Create your views here.
 
 def about_us(request):
-# View to display the About Us page with hotel information.
+# Display on the about page
     about_info = AboutUs.objects.first()
-    return render(request, 'about/about-us.html', 
-                  {'about_info': about_info})
+    return render(request, "about/about.html", {"about_info": about_info})
--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('about/', include("about.urls"), name="about-urls"),
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
