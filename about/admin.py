5cb2d815575a3e47b74ea6ebe459c5663635ad09
Verity Allison <verityalls@gmail.com>
2026-02-13 11:50:32 +0000

    add aboutus model to about admin.py

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import AboutUs
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
+@admin.register(AboutUs)
+class AboutUsAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ("description",)
