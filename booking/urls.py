4249c9d48acb1243142ac8cddfa77e2c4b1fc645
Verity Allison <verityalls@gmail.com>
2026-02-16 13:53:42 +0000

    Remove unused import

--- booking/urls.py
+++ booking/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from .views import BookingList, RoomList, AboutUsView
 from .views import BookingList, RoomList, PreviousBookingList
-
 
 urlpatterns = [
     path('', RoomList.as_view(), name='room_list'),
-    path('about/', AboutUsView.as_view(), name='about_us'),
     path('bookings/', BookingList.as_view(), name='booking_list'),
     path('manage-bookings/', BookingList.as_view(), name='manage_bookings'),
     path('previous-bookings/', PreviousBookingList.as_view(), name='previous_bookings'),
